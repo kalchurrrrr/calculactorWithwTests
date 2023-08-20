4168306ff1287652aaa71809094ef7a209db6eb1
kalchurrrrr <nikolaykalchu@gmail.com>
2023-08-20 21:09:26 +0300

    Initial commit

---
+++ src/main/java/com/tests/calculactorWithwTests/CalculactorWithwTestsApplication.java
@@ -0,0 +1,13 @@
+package com.tests.calculactorWithwTests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculactorWithwTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculactorWithwTestsApplication.class, args);
+	}
+
+}
