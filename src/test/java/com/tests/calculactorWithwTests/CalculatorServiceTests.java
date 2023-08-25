e4190bd604938bf4fc0a99534fef32c5aaf1f2ce
kalchurrrrr <nikolaykalchu@gmail.com>
2023-08-25 16:27:43 +0300

    Внёс правки

--- src/test/java/com/tests/calculactorWithwTests/CalculatorServiceTests.java
+++ src/test/java/com/tests/calculactorWithwTests/CalculatorServiceTests.java
@@ -3,6 +3,13 @@ package com.tests.calculactorWithwTests;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.stream.Stream;
 
 public class CalculatorServiceTests {
 
@@ -40,5 +47,40 @@ public class CalculatorServiceTests {
     @Test
     public void testDivideByZero() {
         Assertions.assertThrows(IllegalArgumentException.class, () -> calculatorService.divide(5, 0));
+    }
+    @ParameterizedTest
+    @ValueSource(ints = {2, 5, 10})
+    public void testAddMultipleValues(int num) {
+        int result = calculatorService.add(num, num);
+        Assertions.assertEquals(2 * num, result);
+    }
+
+    @ParameterizedTest
+    @MethodSource("com.tests.calculactorWithwTests.CalculatorServiceTests#subtractArguments")
+    public void testSubtractMultipleValues(int num1, int num2) {
+        int result = calculatorService.subtract(num1, num2);
+        Assertions.assertEquals(num1 - num2, result);
+    }
+
+    static Stream<Arguments> subtractArguments() {
+        return Stream.of(
+                Arguments.of(10, 5),
+                Arguments.of(8, 3),
+                Arguments.of(20, 10)
+        );
+    }
+
+    @ParameterizedTest
+    @CsvSource({"2, 3, 6", "5, 5, 25", "10, 4, 40"})
+    public void testMultiplyMultipleValues(int num1, int num2, int expected) {
+        int result = calculatorService.multiply(num1, num2);
+        Assertions.assertEquals(expected, result);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"4, 2, 2", "10, 2, 5", "20, 10, 2"})
+    public void testDivideMultipleValues(int num1, int num2, int expected) {
+        int result = calculatorService.divide(num1, num2);
+        Assertions.assertEquals(expected, result);
     }
 }
